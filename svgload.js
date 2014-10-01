44f253c24bfe7ad8b27b65b473b4ef6b73b0516b
Patrik Hartwig <pat.ggf@gmail.com>
2014-10-01 14:28:53 +1000

    added clone of sag elem

--- svgload.js
+++ svgload.js
@@ -1,16 +1,17 @@
-	var svgLoad = {
-		init: function(filePath) {
-			if(!$) return false;
-			if(localStorage.getItem('_svgLoad_data') != null) { svgLoad.build(); return true; } 
-			$.get(filePath, function(data){ localStorage.setItem('_svgLoad_data', data); svgLoad.build(); });
-		},
-		build: function(){
-			var data = localStorage.getItem('_svgLoad_data');
-			$(function(){
-				$('body').append('<div style="display:none;">'+data+'</div>');
-				$('svg[data-svg]').each(function(){
-					$(this).append($($(this).attr('data-svg')));
-				});
+var svgLoad = {
+	init: function(filePath) {
+		if(!$) return false;
+		if(localStorage.getItem('_svgLoad_data') != null) { svgLoad.build(); return true; } 
+		$.get(filePath, function(data){ localStorage.setItem('_svgLoad_data', data); svgLoad.build(); });
+	},
+	build: function(){
+		var data = localStorage.getItem('_svgLoad_data');
+		$(function(){
+			$('body').append('<div style="display:none;">'+data+'</div>');
+			$('svg[data-svg]').each(function(i){
+				console.log($($(this).attr('data-svg')));
+				$(this).append($($(this).attr('data-svg')).clone());
 			});
-		}
+		});
 	}
+}
